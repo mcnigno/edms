c0df1dda60efe663dfbacb7539988e8779b8a77a
Mc Nigno <mcnigno@gmail.com>
2018-08-04 20:02:59 +0200

    setting doctypes

--- edms/enterprise_document_management_system/doctype/bap_code/bap_code.js
+++ edms/enterprise_document_management_system/doctype/bap_code/bap_code.js
@@ -1,22 +1,42 @@
 // Copyright (c) 2018, QuasarPM.com and contributors
 // For license information, please see license.txt
-
+//frappe.ui.form.on('BAP Code', {
+//	refresh: function(frm) {
+/*
 frappe.ui.form.on("BAP Code", "data_1",
-    function(frm) {
-        frappe.call({
-            "method": "frappe.client.get",
-            args: {
-                doctype: "Document List",
-                name: frm.doc.unit
-            },
-            callback: function (data) {
+	function(frm) {
+		frappe.call({
+			"method": "frappe.client.get",
+			args: {
+				doctype: "Document List",
+				name: frm.doc.data_1
+			},
+			callback: function (data) {
 				
-                frappe.model.set_value(frm.doctype,
-                    frm.docname, "bap_code",
-                    data.message.unit
-                    + (data.message.document_name ?
-                        (" " + data.message.document_name) : ""))
-            }
+				frappe.model.set_value(frm.doctype,
+					frm.docname, "bap_code",
+					data.message.document_name
+					+ (data.message.document_name ?
+						(" " + data.message.document_name) : ""))
+			}
 		})
-		console.log('js functions ........')
-    });
+		console.log('setting unit ........')
+		frappe.call({
+			"method": "frappe.client.get",
+			args: {
+				doctype: "Document List",
+				name: frm.doc.data_1
+			},
+			callback: function (data) {
+				
+				frappe.model.set_value(frm.doctype,
+					frm.docname, "unit",
+					data.message.unit)
+			}
+		})
+	}
+);
+*/
+
+//	}
+//});
