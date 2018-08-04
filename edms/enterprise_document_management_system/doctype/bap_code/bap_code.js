b7a9cbfbd709c97975a23194919c02ebc7af0b7c
Mc Nigno <mcnigno@gmail.com>
2018-08-04 10:40:16 +0200

    js on bap code

--- edms/enterprise_document_management_system/doctype/bap_code/bap_code.js
+++ edms/enterprise_document_management_system/doctype/bap_code/bap_code.js
@@ -1,8 +1,22 @@
 // Copyright (c) 2018, QuasarPM.com and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('BAP Code', {
-	refresh: function(frm) {
-
-	}
-});
+frappe.ui.form.on("BAP Code", "data_1",
+    function(frm) {
+        frappe.call({
+            "method": "frappe.client.get",
+            args: {
+                doctype: "Document List",
+                name: frm.doc.unit
+            },
+            callback: function (data) {
+				
+                frappe.model.set_value(frm.doctype,
+                    frm.docname, "bap_code",
+                    data.message.unit
+                    + (data.message.document_name ?
+                        (" " + data.message.document_name) : ""))
+            }
+		})
+		console.log('js functions ........')
+    });
